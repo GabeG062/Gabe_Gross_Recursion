9cf9f04f8067bf07efc07d7f0ce2c7c1c3081e53
starman062 <gabrielgross222@gmail.com>
2026-05-02 20:09:31 -0700

    Palindromes Done

--- recursionlab_master/Recursion_Core5/Palindromes/Program.cs
+++ recursionlab_master/Recursion_Core5/Palindromes/Program.cs
@@ -13,57 +13,19 @@ namespace Palindromes
             Console.WriteLine(isTrue);
             
         }
+
         static bool PrintPalindrome(string s)
         {
-            bool palindrome = true;
-            string[] letters = s.Select(c => c.ToString()).ToArray();
-            if (s.Length > 1)
+            if (string.IsNullOrEmpty(s) || s.Length == 1)
             {
-                return PrintPalindrome(s.Substring(1));
+                return true;
             }
-            if (string.IsNullOrEmpty(s))
+
+            if (s[0] != s[s.Length - 1])
             {
-                return palindrome;
+                return false;
             }
-            else if (s.Length == 1)
-            {
-                return palindrome;
-            }
-            else if (letters.Length % 2 == 0)
-            {
-                int split = letters.Length / 2;
-                string[] firstHalf = letters.Take(split).ToArray();
-                string[] secondHalf = letters.Skip(split).ToArray();
-                if (firstHalf.SequenceEqual(secondHalf))
-                {
-                    return palindrome;
-                }
-                else
-                {
-                    palindrome = false;
-                    return palindrome;
-                }
-            }
-            else if (letters.Length % 2 == 1)
-            {
-                int split = letters.Length / 2;
-                string [] evenSplit = letters.Where((value, index) => index != split).ToArray();
-                split = evenSplit.Length / 2;
-                string[] firstHalf = evenSplit.Take(split).ToArray();
-                string[] secondHalf = evenSplit.Skip(split).ToArray();
-                if (firstHalf.SequenceEqual(secondHalf))
-                {
-                    return palindrome;
-                }
-                else
-                {
-                    palindrome = false;
-                    return palindrome;
-                }
-            }
-            
-            
-            return palindrome;
+            return PrintPalindrome(s.Substring(1, s.Length - 2));
         }
     }
 }
