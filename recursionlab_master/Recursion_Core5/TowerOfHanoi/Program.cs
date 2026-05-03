9da8c00741c239b8600673cc17c698057d7f91f9
starman062 <gabrielgross222@gmail.com>
2026-05-03 14:35:19 -0700

    TowerOfHanoi Done

--- recursionlab_master/Recursion_Core5/TowerOfHanoi/Program.cs
+++ recursionlab_master/Recursion_Core5/TowerOfHanoi/Program.cs
@@ -6,12 +6,35 @@ namespace TowerOfHanoi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Insert the number of rings you want to try (with numbers, so 1 instead of one)");
+            int n = Convert.ToInt32(Console.ReadLine());
+            string start = "start";
+            string aux = "aux";
+            string end = "end";
+            
+            TowerOfHanoi(n, start, aux, end);
+            Console.WriteLine($"Number of moves: {Math.Pow(2, n) - 1}");
+
         }
-        static void PrintTriangularNumbers(int n)
+        static void TowerOfHanoi(int n, string start, string aux, string end)
         {
-            Console.WriteLine("0");
-            PrintTriangularNumbers(n - 1);
+            if (n == 0)
+            {
+                return;
+            }
+            if (n == 1)
+            {
+                Console.WriteLine($"move disk {n} from {start} to {end}");
+                return;
+            }
+
+            if (n >= 2)
+            {
+                TowerOfHanoi(n - 1, start, end, aux);
+                Console.WriteLine($"move disk {n} from {start} to {end}");
+                TowerOfHanoi(n - 1, aux,start, end);
+            }
+            
         }
     }
 }
